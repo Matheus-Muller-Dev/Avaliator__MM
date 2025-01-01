ee6aadb11d7b7ee272a3835a5c7bd8f45e572e6b
Matheus Muller <matheusmiiller44@gmail.com>
2024-12-31 21:26:05 -0300

    update netlify.toml e public

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    root: './public',
+    build: {
+        outDir: 'dist',  // Certifique-se de que os arquivos de build vão para 'dist'
+        rollupOptions: {
+          input: '/index.html'  // Especifique explicitamente o arquivo de entrada
+        }  // Certifique-se de que o Vite está gerando a pasta dist
+  },
   server: {
     open: '/index.html', // Isso abrirá o arquivo index.html no navegador automaticamente
   },
